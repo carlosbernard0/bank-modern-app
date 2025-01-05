1b940009e37c8accc203ca6aac570a9bc9659be5
carlosbernard0 <carlosbernardo774@gmail.com>
2025-01-05 08:34:18 -0300

    structure and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Certifique-se de que os caminhos estão corretos
+  ],
   mode: "jit",
   theme: {
     extend: {
@@ -13,15 +15,7 @@ export default {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
-    },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
-    },
+    }
   },
   plugins: [],
 }
